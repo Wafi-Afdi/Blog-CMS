1f8b1f1e6cd49f9d48aaa5b5581a2a0a8a8a57a0
Wafi <wafiafdi@gmail.com>
2023-03-03 23:26:05 +0700

    comment form and comment done

--- components/author.jsx
+++ components/author.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
-const author = () => {
+const author = ({ author }) => {
   return (
-    <div>author</div>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className='flex justify-center'>
+        <div className="absolute -top-14">
+          <Image
+            unoptimized
+            alt={author.name}
+            height={100}
+            width={100}
+            className="align-middle rounded-full"
+            src={author.photo.url}
+          />
+        </div>
+      </div>
+      <h3 className="text-white mt-4 mb-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-ls">{author.bio}</p>
+    </div>
+      
   )
 }
 
--- components/commentsForm.jsx
+++ components/commentsForm.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { submitComment } from '@/services'
 
-const commentsForm = () => {
+const commentsForm = ({slug}) => {
+  const [error, setError] = useState(false)
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef("");
+  const nameEl = useRef("");
+  const emailEl = useRef("");
+  const storeDataEl = useRef("");
+
+ 
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name')
+    emailEl.current.value = window.localStorage.getItem('email')
+  }, [])
+
+  const handleCommentSubmission = () => {
+    setError(false)
+    // get the current value of ref
+    const {checked: storeData} = storeDataEl.current;
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+
+    if(!comment || !name || !email){
+      setError(true);
+      return;
+    }
+
+    const commentObj = {name, email, comment, slug};
+
+    if(storeData){
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
+    } else {
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
+    }
+
+    submitComment(commentObj)
+      .then((res) => {
+        setShowSuccessMessage(true);
+        setTimeout(() =>{
+          setShowSuccessMessage(false)
+        }, 3000)
+      })
+
+  }
+
   return (
-    <div>commentsForm</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 pb-12 mb-8'>
+      <h3 className='text-xl font-semibold mb-8 border-b pb-4'>Leave a Reply</h3>
+      <div className='grid grid-cols-1 gap-4 mb-4'>
+        <textarea ref={commentEl} 
+          className='p-4 outline-none w-full rounded-lg focus:ring-gray-200 bg-gray-100 text-gray-700'
+          placeholder='Comment'
+          name='comment'>
+          
+        </textarea>
+
+      </div>
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4'>
+        <input type='text' ref={nameEl}
+        className='py-2 px-4 outline-none w-full rounded-lg focus:ring-gray-200 bg-gray-100 text-gray-700'
+        placeholder='Name'
+        name='name'
+        >
+        </input>
+        <input type='text' ref={emailEl}
+        className='py-2 px-4 outline-none w-full rounded-lg focus:ring-gray-200 bg-gray-100 text-gray-700'
+        placeholder='Email'
+        name='email'
+        >
+        </input>
+      </div>
+      <div className='grid grid-cols-1 gap-4 mb-4'>
+        <div className=''>
+          <input 
+          ref={storeDataEl} type='checkbox' id='storeData' name='storeData' value='true'
+          />
+          <label className='text-gray-500 cursor-pointer ml-2' htmlFor='storeData'>Save my email and name for the next time I comment</label>
+        </div>
+      </div>
+      {error && <p className='text-xs text-red-500'>All fields are required</p>}
+      <div className='mt-8'>
+        <button 
+        type='button' 
+        onClick={handleCommentSubmission}
+        className='transition duration-500 ease hover:bg-indigo-900 inline-block bg-black text-lg text-white px-8 py-3 cursor-pointer rounded-full'>
+          Post Comment
+        </button>
+        {showSuccessMessage && <span className='text-xl float-right font-semibold mt-3 text-green-500 '>Comment submitted for review</span>}
+      </div>
+    </div>
   )
 }
 
 export default commentsForm
+
